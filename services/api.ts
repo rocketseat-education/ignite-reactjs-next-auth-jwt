f69830d662cf80ae5b1eabf7ea5897f5ff77e4a9
Diego Fernandes <diego.schell.f@gmail.com>
2021-04-29 17:46:13 -0300

    Realizando refresh do token

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,43 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['nextauth.token']}`
+  }
+});
+
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies();
+
+      const { 'nextauth.refreshToken': refreshToken } = cookies;
+
+      api.post('/refresh', {
+        refreshToken,
+      }).then(response => {
+        const { token } = response.data;
+
+        setCookie(undefined, 'nextauth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+  
+        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+
+        api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      });
+    } else {
+      // deslogar o usuario
+    }
   }
 });
